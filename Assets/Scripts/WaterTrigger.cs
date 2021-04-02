da15fb5ed46e0e4f49be480e7cf557a7fafae5a9
GodOfChess <CreativeOrange1@yandex.ru>
2021-04-02 10:51:18 +0500

    Добавление скрипта для физики обьектов

--- Assets/Scripts/WaterTrigger.cs
+++ Assets/Scripts/WaterTrigger.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class WaterTrigger : MonoBehaviour
 {
-    
+    private float power= 50f;
+    private float speed = 5f;
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "physical")
+        {
+            col.GetComponent<Rigidbody>().AddForce(transform.up * power);
+        }
+
+        if (col.tag == "Player")
+        {
+            col.GetComponent<Rigidbody>().AddForce(transform.up * power);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
+        }
+    }
 }
